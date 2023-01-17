8cad16a6e23d0e01a4df4155eea72dc533b8222b
Maartje Oostvogels <moostvogels@hotmail.com>
2023-01-17 21:38:31 +0000

    Debugging

--- public/js/hairystyles.js
+++ public/js/hairystyles.js
@@ -72,6 +72,7 @@ function initHomepage() {
             var result = mockSearchResults[i];
             console.log(`${i+1} - ${result.hairdresser_name} - ${result.location}`);
         }
+    }
     searchButtonElement.addEventListener('click', searchHairStyles);
 }
 
